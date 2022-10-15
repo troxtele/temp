9000c7249cde60d81dabc823fc82a434e092b232
troxtele <105340164+troxtele@users.noreply.github.com>
2022-10-15 22:54:27 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,21 +2,20 @@ const video = document.querySelector("video");
 
 // hide control panel
 video.controls = false;
+
 let value = true;
-const middle = video.duration / 2;
 let a = true;
 
 // video pause/stop in the middle
 video.ontimeupdate = () => {
-  if (middle < video.currentTime && a) {
+  if (parseInt(video.duration / 2) < video.currentTime && a) {
     video.pause();
     a = !a;
-    console.log(a);
     value = !value;
   }
 };
 
-// reload
+// reload after video ended
 video.onended = () => {
   a = !a;
   value = !value;
@@ -24,21 +23,6 @@ video.onended = () => {
 
 // video play/pause while clicked
 video.onclick = () => {
-  console.log(value);
   value ? video.play() : video.pause();
   value = !value;
 };
-
-// const video = document.querySelector("video");
-// video.controls = false;
-// let value = true;
-// video.onloadedmetadata = function () {
-//   half = this.duration / 2;
-//   const stop = setInterval(() => {
-//     if (parseInt(half) == parseInt(this.currentTime)) {
-//       this.pause();
-//       clearInterval(stop);
-//       value = !value
-//     }
-//   }, 1000);
-// };
